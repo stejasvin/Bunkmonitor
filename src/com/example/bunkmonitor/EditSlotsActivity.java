cd3555c555833bdb1dd9d6ca01902b77b525697f
stejasvin <stejasvin@gmail.com>
2014-03-02 23:22:09 +0530

    Tried to change timetable.. not working.. reverting back

--- src/com/example/bunkmonitor/EditSlotsActivity.java
+++ src/com/example/bunkmonitor/EditSlotsActivity.java
@@ -1,11 +1,7 @@
 package com.example.bunkmonitor;
 
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 /**
  * Created by stejasvin on 1/1/14.
@@ -16,17 +12,51 @@ public class EditSlotsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.edit_slots);
+        setContentView(R.layout.edit_slots_backup);
 
-        final EditText etMon = (EditText)findViewById(R.id.slots_mon);
-        final EditText etTue = (EditText)findViewById(R.id.slots_tue);
-        final EditText etWed = (EditText)findViewById(R.id.slots_wed);
-        final EditText etThurs = (EditText)findViewById(R.id.slots_thurs);
-        final EditText etFri = (EditText)findViewById(R.id.slots_fri);
-        final EditText etSat = (EditText)findViewById(R.id.slots_sat);
-        final EditText etSun = (EditText)findViewById(R.id.slots_sun);
+//        final EditText etMon = (EditText)findViewById(R.id.slots_mon);
+//        final EditText etTue = (EditText)findViewById(R.id.slots_tue);
+//        final EditText etWed = (EditText)findViewById(R.id.slots_wed);
+//        final EditText etThurs = (EditText)findViewById(R.id.slots_thurs);
+//        final EditText etFri = (EditText)findViewById(R.id.slots_fri);
+//        final EditText etSat = (EditText)findViewById(R.id.slots_sat);
+//        final EditText etSun = (EditText)findViewById(R.id.slots_sun);
+        String slots[] = {"A","B","C","D","E","G","P","Q","R","S","Extras"};
+//        TableLayout tlSlots = (TableLayout)findViewById(R.id.slots_tl);
+//        TableRow.LayoutParams cp = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1f);
+//        TableRow.LayoutParams trp = new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        TableRow.LayoutParams ip = new TableRow.LayoutParams(50, 50);
+//        for(int i=0;i<slots.length;i++){
+//            TableRow row = new TableRow(this);
+//            row.setLayoutParams(trp);
+//            TextView tv = new TextView(this);
+//            tv.setTextColor(getResources().getColor(android.R.color.white));
+//            tv.setText(slots[i]);
+//            tv.setGravity(Gravity.CENTER);
+//            tv.setLayoutParams(cp);
+//            row.addView(tv);
+//            for(int j=0;j<7;j++){
+//
+//                final Button cb = new Button(this);
+//
+////                cb.setOnClickListener(new View.OnClickListener() {
+////                    @Override
+////                    public void onClick(View v) {
+////                        if(cb.getDrawable()==getResources().getDrawable(R.drawable.e_chalkbox))
+////                            cb.setImageDrawable(getResources().getDrawable(R.drawable.f_chalkbox));
+////                        else
+////                            cb.setImageDrawable(getResources().getDrawable(R.drawable.e_chalkbox));
+////                    }
+////                });
+////                cb.setImageDrawable(getResources().getDrawable(R.drawable.e_chalkbox));
+//                cb.setLayoutParams(cp);
+//                //cb.setGravity(Gravity.CENTER);
+//                row.addView(cb);
+//            }
+//            tlSlots.addView(row);
+//        }
 
-        final SharedPreferences mPrefs = getSharedPreferences(
+        /*final SharedPreferences mPrefs = getSharedPreferences(
                 "bunkmonitor.SHARED_PREF", 0);
         etMon.setText(mPrefs.getString("MONDAY",""));
         etTue.setText(mPrefs.getString("TUESDAY",""));
@@ -50,6 +80,6 @@ public class EditSlotsActivity extends Activity {
                 mEditor.putString("SUNDAY", etSun.getText().toString()).commit();
                 finish();
             }
-        });
+        });*/
     }
 }
